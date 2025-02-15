6e394551f1a089a92f83fc002bc74d5f007ccdce
azozocode <josephbarrigah@gmail.com>
2025-02-15 23:30:26 +0000

    add a channel console project

---
+++ src/Console/Core.Channel/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Threading.Channels;
+
+namespace Core.Channel;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var channel = System.Threading.Channels.Channel.CreateUnbounded<int>();
+        using var cancellationToken = new CancellationTokenSource();
+        
+      var producer =  Task.Run(async() =>
+        { 
+           await ChannelService.ProduceAsync(channel);
+        }, cancellationToken.Token);
+
+       var consumer = Task.Run(async () =>
+        {
+            await ChannelService.ConsumeAsync(channel);
+        },cancellationToken.Token);
+
+        await Task.WhenAll(producer,consumer);
+    }
+}
---
+++ src/Console/Core.Delegate/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Core.Delegate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+    }
+
+}
